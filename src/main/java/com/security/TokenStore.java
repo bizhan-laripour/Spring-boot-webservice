63d3ddfd738789bf40aa492b379dc9692a2f97f2
laripour <bizhan.laripour.herat@gmail.com>
2019-05-13 19:07:40 +0430

    security

---
+++ src/main/java/com/security/TokenStore.java
@@ -0,0 +1,26 @@
+package com.security;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.boot.context.properties.ConfigurationBeanFactoryMetadata;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class TokenStore {
+
+
+    public static String createToken(){
+        SignatureAlgorithm algorithm = SignatureAlgorithm.ES256;
+        JwtBuilder jwtBuilder = Jwts.builder()
+//                .setId("something")
+//                .setAudience("something")
+//                .setSubject("something")
+                .setExpiration(new Date(System.currentTimeMillis()+SecurityConstants.EXPIRATION_TIME))
+                .signWith(algorithm , SecurityConstants.SECRET);
+        return SecurityConstants.TOKEN_PREFIX + jwtBuilder.compact();
+    }
+}
---
+++ src/main/java/com/security/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(SecurityConstants.SIGN_UP_URL).permitAll()
+                .antMatchers("/v2/api-doc" , "/configuration/**" , "/swagger*/**" , "/webjars/**").permitAll()
+                .anyRequest().authenticated().
+                and().addFilter(new JwtAutherizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
+
+
+
